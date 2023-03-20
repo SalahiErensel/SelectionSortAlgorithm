21e25ae430eadb628f08c31127209855dc45531b
Salahi Erensel <salahi_erensel@hotmail.com>
2023-03-20 16:04:34 +0300

    Add project files.

---
+++ SelectionSortAlgorithm/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SelectionSortAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j,tmp;
+            int[] array = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            for (i=0; i<array.Length; i++)
+            {
+                int minimum = i;
+                for (j = i; j<array.Length; j++)
+                {
+                    if (array[j] < array[minimum])
+                    {
+                        minimum = j;
+                    }
+                }
+                tmp = array[minimum];
+                array[minimum] = array[i];
+                array[i] = tmp;
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
